ee555f5a2d37a06f90a3f92776cc6818eca76854
veerprataps890 <veerpratap.s.890@gmail.com>
2026-02-15 16:07:55 +0530

    Day 11 - Matrix Addition

---
+++ DAY11/Matrix_Addition.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    // Input rows and columns
+    scanf("%d %d", &m, &n);
+
+    int A[m][n], B[m][n], Sum[m][n];
+
+    // Input first matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Input second matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Add matrices
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Print result
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
