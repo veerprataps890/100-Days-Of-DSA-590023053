ffb46359afc0c3c90f552dd9102cf131fe79c5f4
veerprataps890 <veerpratap.s.890@gmail.com>
2026-02-15 16:17:18 +0530

    Day 14 - Check Identity Matrix

---
+++ DAY14/Check_Identity_Matrix.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[n][n];
+    int isIdentity = 1;
+
+    // Input matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check identity condition
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+
+            if(i == j && matrix[i][j] != 1) {
+                isIdentity = 0;
+                break;
+            }
+
+            if(i != j && matrix[i][j] != 0) {
+                isIdentity = 0;
+                break;
+            }
+        }
+    }
+
+    if(isIdentity)
+        printf("Identity Matrix");
+    else
+        printf("Not an Identity Matrix");
+
+    return 0;
+}
